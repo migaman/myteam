d515365542999446d416b5d25a43c752ef56e65e
migaman <mikee@bluewin.ch>
2018-01-13 18:51:20 +0100

    Port from variable

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var http = require('http');
 
+const PORT = process.env.PORT;
+
 http.createServer(function (req, res) {
 	console.log("great!");
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World!');
-}).listen(8080); 
+}).listen(PORT); 
 
 
 
