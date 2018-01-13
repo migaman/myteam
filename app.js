7d7a71d7e3c88c178b8ee25a4f84e96bc82302b4
migaman <mikee@bluewin.ch>
2018-01-13 19:19:41 +0100

    Show release version

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ var http = require('http');
 
 const PORT = process.env.PORT;
 
+//Needs feature Dyno Metadata (https://stackoverflow.com/questions/7917523/how-do-i-access-the-current-heroku-release-version-programmatically)
+const VERSION = process.env.HEROKU_RELEASE_VERSION;
+
 http.createServer(function (req, res) {
 	console.log("great!");
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    res.end('Hello World! Release: ' + VERSION);
 }).listen(PORT); 
 
 
