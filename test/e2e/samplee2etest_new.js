65d00de598b79ad461f62ed03e7634ee6602ce8f
migaman <mikee@bluewin.ch>
2018-02-24 16:19:20 +0100

    Configure e2e tests

---
+++ test/e2e/samplee2etest_new.js
@@ -0,0 +1,43 @@
+'use strict';
+const {Builder, By, until, promise} = require('selenium-webdriver');
+var assert = require('assert');
+
+describe('samplee2etest', function(){
+	this.timeout(15000);
+	let driver;
+	
+	beforeEach(async function() {
+		driver = await new Builder().forBrowser('firefox').build();
+	  });
+	  
+	  afterEach(async function() {
+		await driver.quit();
+	});
+	
+	it('demo', async function() {
+		var application_host = 'http://localhost:3000';
+		console.log(driver.getTitle());
+		await driver.get(application_host);
+		await driver.wait(until.titleIs('Express'), 1000);
+		await driver.findElement(By.css('body'));
+		await driver.findElement(By.css('body > h1'));
+		
+		await driver.get("http://localhost:3000");
+		let url = await driver.getCurrentUrl();
+		assert.equal(url, "http://localhost:3000/");
+		  
+		  
+		let title = await driver.getTitle();
+		assert.equal(title, "Express");
+		
+		let titleH1 = await driver.findElement(By.css('body > h1')).getText();
+		assert.equal(titleH1, "Express");
+		
+		let welcome = await driver.findElement(By.css('body > p')).getText();
+		assert.equal(welcome, "Welcome to Express");
+	
+	});
+	
+});
+
+
