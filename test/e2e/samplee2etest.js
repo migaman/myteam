f1b1c53c955509051bcaacd95a013c58121c4ada
migaman <mikee@bluewin.ch>
2018-02-25 07:49:34 +0100

    Tests

--- test/e2e/samplee2etest.js
+++ test/e2e/samplee2etest.js
@@ -1,29 +1,49 @@
+'use strict';
+const {Builder, By, until, promise} = require('selenium-webdriver');
 var assert = require('assert');
+const firefox = require('selenium-webdriver/firefox');
 
-module.exports = {
-  /*desiredCapabilities : {
-    name : 'test-Name'
-  },*/
-  
-  demoTest : function (client) {
-    //assert.equal(client.options.desiredCapabilities.name, 'test-Name');
-	console.log("URL: " + client.launchUrl);
+describe('samplee2etest', function(){
+	this.timeout(10000);
+	let driver;
 	
-	client
-	.url(client.launchUrl)
-	.waitForElementVisible('body', 1000)
-	//.waitForElementVisible('body > h1', 1000)
-	.pause(1000);
-	client.assert.title("Express")
-	client.assert.visible("body > h1")
-	client.assert.visible("body > p")
-	client.assert.containsText("body > h1", "Express");
-	client.assert.containsText("body > p", "Welcome to Express");
-  },
+	beforeEach(async function() {
+	
+		driver = await new Builder()
+			.forBrowser('firefox')
+			.setFirefoxOptions(new firefox.Options().headless())
+			.build();
+	  });
+	  
+	  afterEach(async function() {
+		await driver.quit();
+	});
+	
+	it('demo', async function() {
+		var application_host = 'http://localhost:3000';
+				
+		await driver.get(application_host);
+		await driver.wait(until.titleIs('Express'), 1000);
+		await driver.findElement(By.css('body'));
+		await driver.findElement(By.css('body > h1'));
+		
+		await driver.get("http://localhost:3000");
+		let url = await driver.getCurrentUrl();
+		assert.equal(url, "http://localhost:3000/");
+		  
+		  
+		let title = await driver.getTitle();
+		assert.equal(title, "Express");
+		
+		let titleH1 = await driver.findElement(By.css('body > h1')).getText();
+		assert.equal(titleH1, "Express");
+		
+		let welcome = await driver.findElement(By.css('body > p')).getText();
+		assert.equal(welcome, "Welcome to Express");
+		
+	
+	});
+	
+});
 
-  after : function(client) {
-    client.end(function() {
 
-    });
-  }
-};
--- test/unit/sampleunittest.js
+++ test/unit/sampleunittest.js
@@ -1,12 +1,14 @@
+'use strict';
 var assert = require('assert');
-module.exports = {
-  
-  exampleArrayTest : function () {
-	assert.equal([1,2,3].indexOf(4), -1);
-  },
-  
-  exampleNumberTest : function () {
-	assert.equal(1, 1);
-  }
 
-};
+describe('sampleunittest', function(){
+
+  it('exampleArrayTest', function(){
+	assert.equal([1,2,3].indexOf(4), -1);
+  });
+  
+  it('exampleNumberTest', function(){
+	assert.equal(1, 1);
+  });
+});
+
